ade0d35ec668604e2074a4227ff0825264f2b0dc
Abhishek Singh <88226074+abhi01-01@users.noreply.github.com>
2022-04-15 15:59:18 +0530

    added env

--- src/Socket.js
+++ src/Socket.js
@@ -7,5 +7,5 @@ export const initSocket = async () => {
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io("http://localhost:3001" , options)
+    return io(process.env.REACT_APP_BACKEND_URL, options)
  };
